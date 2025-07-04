722aa52aa11385a55e98397d9bcb36fc9cbbbb35
pratikwaghade <pratikwaghade31@gmail.com>
2025-07-04 14:55:59 +0530

    first commit

---
+++ CalculatorWebAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("add")]
+        public IActionResult Add(double num1 , double num2)
+        {
+            return Ok(num1 + num2);
+        }
+
+        [HttpGet("subtract")]
+        public IActionResult Subtract(double num1, double num2)
+        {
+            return Ok(num1 - num2);
+        }
+
+        [HttpGet("multiply")]
+        public IActionResult Multiply(double num1, double num2)
+        {
+            return Ok(num1 * num2);
+        }
+
+        [HttpGet("divide")]
+        public IActionResult Divide(double num1, double num2)
+        {
+            if(num2 == 0)
+            {
+                return BadRequest("Cannot divide by zero");
+            }
+
+            return Ok(num1 / num2);
+        }
+
+    }
+}
